8af953487f3a7e7acc0d9e8a2dc68a8791dedcc8
Mohammed HAJHOUJ <m.hajhouj@gmail.com>
2023-02-04 00:44:12 +0100

    add items to numbers

--- src/test/java/com/hajhouj/oss/fastsico/helper/SortArrayWithIndexesTest.java
+++ src/test/java/com/hajhouj/oss/fastsico/helper/SortArrayWithIndexesTest.java
@@ -12,8 +12,8 @@ public class SortArrayWithIndexesTest {
    */
   @Test
   public void testSortWithIndexes() {
-    int[] numbers = {3, 5, 1, 2, 4};
-    int[] expectedSortedIndexes = {2, 3, 0, 4, 1};
+    int[] numbers = {3, 5, 1, 2, 4, 6};
+    int[] expectedSortedIndexes = {2, 3, 0, 4, 1, 5};
     int[] sortedIndexes = SortArrayWithIndexes.sortWithIndexes(numbers);
     assertArrayEquals(expectedSortedIndexes, sortedIndexes);
   }
